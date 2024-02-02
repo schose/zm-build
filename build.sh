caece1de87fda8534900f4901fd1a88ada2150f3
Andreas Roth <andreas@batchworks.de>
2024-02-02 22:12:54 +0100

    update and test..

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ mkdir installer-build
 cd installer-build    
 git clone https://github.com/Zimbra/zm-build.git
 cd zm-build
-cp ../../config.build .
+cp /tmp/config.build .
 sudo mkdir -p /root/.ivy2/cache
 sudo ENV_CACHE_CLEAR_FLAG=true ./build.pl
 # --ant-options -DskipTests=true --git-default-tag=10.0.0-GA --build-release-no=10.0.0 --build-type=FOSS --build-release=NIKOLATESLA --build-release-candidate=GA --build-thirdparty-server=files.zimbra.com --build-no=3969 --no-interactive
